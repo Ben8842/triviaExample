6abd690d200692d37ac5c60650cde557caf83921
Ben8842 <z87e99b44r55a@gmail.com>
2021-01-15 12:38:37 -0500

    beginning commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Hello Trivia</header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startButton: true,
+      categoryDisplay: false,
+    };
+  }
+  //constructor(props) {}
+  //componentDidMount() {}
+  handleStart() {
+    this.setState({
+      startButton: false,
+      categoryDisplay: true,
+    });
+  }
+
+  render() {
+    var { startButton, categoryDisplay } = this.state;
+    const triviaStartButton = (
+      <div>
+        <button onClick={() => this.handleStart()}>Start Trivia</button>
+      </div>
+    );
+
+    const categoryChoices = (
+      <div>
+        <h3>Choose Category</h3>
+      </div>
+    );
+
+    return (
+      <div>
+        {startButton ? triviaStartButton : null}
+        {categoryDisplay ? categoryChoices : null}
+      </div>
+    );
+  }
 }
 
 export default App;
